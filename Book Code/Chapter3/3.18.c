ac24034c05cc4f4f74d55fde6e5c52b113c8932a
fnilsson <gusnilfral@student.gu.se>
2025-02-05 11:20:14 +0100

    Add simple input validations

--- Book Code/Chapter3/3.18.c
+++ Book Code/Chapter3/3.18.c
@@ -20,10 +20,17 @@ new balance and message "Credit limit exceeded."
  */
 #include <stdio.h>
 
+// Define constants
+#define MIN_ACCOUNT 1
+#define MAX_ACCOUNT 100
+#define MINIMUM 0
+#define MAX_CHARGES 5000
+#define MAX_LIMIT 10000
+#define MAX_START_BALANCE 100000
 
 int main () {
 
-
+// Initialize variables
 int accountNumber = 0;
 double beginningBalance = 0;
 double totalCharges = 0;
@@ -31,7 +38,9 @@ double credits = 0;
 double creditLimit = 0;
 
 
+    // While-loop to run until user inputs -1 to end
     while (accountNumber != -1) {
+
 
       printf("Enter account number (-1 to end): \n");
       scanf("%d", &accountNumber);
@@ -40,17 +49,36 @@ double creditLimit = 0;
         break;
       }
 
+      if (accountNumber < MIN_ACCOUNT || accountNumber > MAX_ACCOUNT) {
+        printf("Invalid input. Please provide an integer between 1 and 100\n");
+        continue;
+      }
+
+
       printf("Enter the beginning balance: \n");
-      scanf("%lf", &beginningBalance);
+      if (scanf("%lf", &beginningBalance) != 1 || beginningBalance < MINIMUM || beginningBalance > MAX_START_BALANCE) {
+        printf("Invalid input. please provide an integer between %d and %d\n", MINIMUM, MAX_START_BALANCE);
+        continue;
+      }
 
       printf("Enter the total charges: \n");
-      scanf("%lf", &totalCharges);
+      if (scanf("%lf", &totalCharges) != 1 || totalCharges < MINIMUM || totalCharges > MAX_CHARGES) {
+        printf("Invalid charge amount. Values are at most between %d and %d\n", MINIMUM, MAX_CHARGES);
+        continue;
+      }
+
 
       printf("Enter the total credits: \n");
-      scanf("%lf", &credits);
+      if (scanf("%lf", &credits) != 1 || credits < MINIMUM) {
+        printf("Invalid input. Must be minimum %d\n", MINIMUM);
+        continue;
+      }
 
       printf("Enter credit limit: \n");
-      scanf("%lf", &creditLimit);
+      if (scanf("%lf", &creditLimit) != 1 || creditLimit < MINIMUM || creditLimit > MAX_LIMIT) {
+        printf("Invalit limit amount. Please provide input between %d and %d", MINIMUM, MAX_LIMIT);
+        continue;
+      }
 
       printf("Account:\t%d\n", accountNumber);
       printf("Credit Limit: %.2lf\n", creditLimit);
