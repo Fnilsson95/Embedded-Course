3e4ff206e8f2cb8a0a440b76b48ed042f43ac674
fnilsson <gusnilfral@student.gu.se>
2025-02-05 10:20:30 +0100

    Add initial file and exercise description for 3.18

---
+++ Book Code/Chapter3/3.18.c
@@ -0,0 +1,32 @@
+//
+// Created by Fredrik Nilsson on 2025-02-05.
+//
+
+
+/* Exercise Description:
+Develop a c program that will determine if a department store customer has exceeded the credit limit on a charge account.
+For each customer, the following facts are available:
+a) Account number
+b) Balance at the beginning of the month
+c) Total of all items charged by this customer this month
+d) Total of all credits applied to this customer's account this month
+e) Allowed credit limit
+
+The program should input each of these facts, calculate the new balance (= beginning balance + charges - credits),
+determine if the new balance exceeds the customer's credit limit.
+For those customers whose credit limit is exceeded, the program should display the customer's account number, credit limit,
+new balance and message "Credit limit exceeded."
+
+ */
+#include <stdio.h>
+
+
+int main () {
+
+
+
+
+
+
+  return 0;
+}
