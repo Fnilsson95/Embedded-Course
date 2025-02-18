698299c4584b0e49957b43a7317662d4ba579f42
fnilsson <gusnilfral@student.gu.se>
2025-02-18 09:49:47 +0100

    Add folder and main file for Bonus assignment WP6
    Main file containts exercise description and short numerical list of my own thoughts

---
+++ Bonus Assignments/WP6/main.c
@@ -0,0 +1,42 @@
+// Bonus Assignment WP6
+// Fredrik Nilsson
+
+/* Exercise Description
+
+Write a program that reads 5 strings from the console,
+stores them in an array and finds the shortest string
+
+Function:
+Program should contain a function that takes the array as parameter,
+finds the shortest string, remove it and return the array that is 
+shorter by one element.
+
+
+The program should write the string that has been removed to the console
+Also print the elements of the array before and after removal
+
+
+
+1. Prompt user
+2. Read and store user input (Either specific size or store via Malloc for dynamic allocation?)
+3. Validation --> 5 strings (Certain size, only letters?)
+4. Create function
+5. strcpr(array[i] and array[j] ? or array[i] and array[i+1] or pointers?)
+*/
+
+
+
+#include <stdio.h>
+
+void compareStrings();
+
+int main() {
+
+
+
+    return 0;
+}
+
+
+void compareStrings() {}
+
